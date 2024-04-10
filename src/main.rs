76551b21fe877c830906a54482ad79bdd0313bad
Kriemhild Gretchen <madoka.karamazov@gmail.com>
2024-04-09 19:38:15 -0700

    copy stuff from mtheo
    set up env with new 'schiz' db and user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,106 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::{Arc, RwLock};
+
+#[derive(Clone)]
+struct AppState {
+    db: sqlx::PgPool,
+    jinja: Arc<RwLock<minijinja::Environment<'static>>>,
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+    let state = {
+        let db = {
+            let url = std::env::var("PG_URL").expect("read PG_URL env");
+            sqlx::PgPool::connect(&url).await.expect("connect postgres")
+        };
+        let jinja = {
+            let mut env = minijinja::Environment::new();
+            env.set_loader(minijinja::path_loader("templates"));
+            env.add_filter("repeat", str::repeat);
+            Arc::new(RwLock::new(env))
+        };
+        AppState { db, jinja }
+    };
+    let router = router(state);
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:7878")
+        .await
+        .expect("listen on 7878");
+    axum::serve(listener, router).await.expect("serve axum")
+}
+
+fn router(state: AppState) -> axum::Router {
+    use axum::routing::{get, post};
+    axum::Router::new()
+        .route("/", get(index))
+        .layer(trace())
+        .with_state(state)
+}
+
+use tower_http::{
+    classify::{ServerErrorsAsFailures, SharedClassifier},
+    trace::TraceLayer,
+};
+
+fn trace() -> TraceLayer<SharedClassifier<ServerErrorsAsFailures>> {
+    use tower_http::trace::{DefaultMakeSpan, DefaultOnResponse};
+    use tracing::Level;
+    tracing_subscriber::fmt()
+        .with_max_level(Level::DEBUG)
+        .init();
+    TraceLayer::new_for_http()
+        .make_span_with(DefaultMakeSpan::new().level(Level::DEBUG))
+        .on_response(DefaultOnResponse::new().level(Level::DEBUG))
+}
+
+// individual http request handlers follow
+
+fn dev() -> bool {
+    std::env::var("DEV").is_ok_and(|v| v == "1")
+}
+
+fn render(
+    lock: Arc<RwLock<minijinja::Environment<'_>>>,
+    name: &str,
+    ctx: minijinja::value::Value,
+) -> String {
+    if dev() {
+        let mut env = lock.write().expect("write jinja env");
+        env.clear_templates();
+    }
+    let env = lock.read().expect("read jinja env");
+    let tmpl = env.get_template(name).expect("get jinja template");
+    tmpl.render(ctx).expect("render template")
+}
+
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
+
+fn http_code(code: StatusCode, msg: &str) -> Response {
+    (code, msg.to_owned()).into_response()
+}
+
+fn bad_request(msg: &str) -> Response {
+    http_code(StatusCode::BAD_REQUEST, msg)
+}
+
+fn conflict(msg: &str) -> Response {
+    http_code(StatusCode::CONFLICT, msg)
+}
+
+fn unauthorized() -> Response {
+    http_code(StatusCode::UNAUTHORIZED, "unauthorized")
+}
+
+use axum::{extract::State, http::HeaderMap, response::Html};
+use axum_extra::extract::cookie::{Cookie, CookieJar, SameSite};
+
+async fn index(
+    State(state): State<AppState>,
+    headers: HeaderMap,
+    mut jar: CookieJar,
+) -> Response {
+    String::from("hello schizo").into_response()
 }
