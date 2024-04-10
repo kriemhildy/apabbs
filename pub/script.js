ec78d09f62f212d78a2f106d217dac4e9a74588d
Kriemhild Gretchen <madoka.karamazov@gmail.com>
2024-04-09 21:03:05 -0700

    variable styling and scrollbar
    return to variables because scrollbar also uses them.

--- pub/script.js
+++ pub/script.js
@@ -1,2 +1,10 @@
 window.onload = function () {
+    // style the scrollbar. this primarily affects chrome on windows
+    if (!navigator.userAgent.includes("Macintosh") && navigator.userAgent.includes("WebKit")) {
+        const mainLink = document.querySelector("link[rel=stylesheet]");
+        const scrollbarLink = document.createElement("link");
+        scrollbarLink.rel = "stylesheet";
+        scrollbarLink.href = "/scrollbar.css";
+        mainLink.after(scrollbarLink);
+    }
 };
