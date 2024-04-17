3b99897039853d003ce684b6996b48fe6c2b9f57
Kriemhild Gretchen <madoka.karamazov@gmail.com>
2024-04-16 20:46:38 -0700

    add argon2 and User::encrypt_password
    this will follow the current OWASP recommendations for password
    encryption.

--- src/user.rs
+++ src/user.rs
@@ -41,17 +41,44 @@ impl User {
         password.len() >= 8 && !lowercase_password.contains(lowercase_name.as_str())
     }
 
+    fn encrypt_password(password: &str) -> (String, String) {
+        // resources used:
+        // modern rust hashing guide: https://www.lpalmieri.com/posts/password-authentication-in-rust/
+        // argon2 docs: https://docs.rs/argon2/latest/argon2/
+        use argon2::{
+            password_hash::{rand_core::OsRng, PasswordHasher, SaltString},
+            Algorithm, Argon2, Params, Version,
+        };
+        let password = password.as_bytes();
+        let salt = SaltString::generate(&mut OsRng);
+
+        // Argon2 with OWASP params
+        let hasher = Argon2::new(
+            Algorithm::Argon2id,
+            Version::V0x13,
+            Params::new(15000, 2, 1, None).expect("builds Argon2 params"),
+        );
+
+        // Hash password to PHC string ($argon2id$v=19$...)
+        let password_hash = hasher
+            .hash_password(password, &salt)
+            .expect("hashes password");
+        (password_hash.to_string(), salt.to_string())
+    }
+
     pub async fn register(&self, tx: &mut PgConnection, name: &str, password: &str) -> User {
         // time zone? utc? password encryption?
         // maybe don't bother with registered_at because we should have a separate
         // 'actions' table (or equivalent) that tracks ip and registrations/logins/logouts.
         // we need a reversable encryption system too (just in case) for stuff like IP maybe.
+        let (password_hash, salt) = User::encrypt_password(password);
         sqlx::query_as(concat!(
-            "UPDATE users SET name = $1, encrypted_password = $2, ",
-            "registered_at = now() WHERE id = $3 RETURNING *"
+            "UPDATE users SET name = $1, password_hash = $2, salt = $3, ",
+            "registered_at = now() WHERE id = $4 RETURNING *"
         ))
         .bind(name)
-        .bind(password)
+        .bind(password_hash)
+        .bind(salt)
         .bind(self.id)
         .fetch_one(&mut *tx)
         .await
