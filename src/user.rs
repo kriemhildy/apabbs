d846b2d8727a676e2e2904ccc0efeeb556f08791
Kriemhild Gretchen <madoka.karamazov@gmail.com>
2024-04-18 20:08:36 -0700

    tweak argon2 params code to be a little cleaner
    consistently call PHC SaltString var "phc_salt_string"

--- src/user.rs
+++ src/user.rs
@@ -60,7 +60,7 @@ impl Credentials {
         SaltString::from_b64(b64_salt).expect("convert B64 str to PHC SaltString")
     }
 
-    fn hash_password(password: &str, salt_string: &SaltString) -> String {
+    fn hash_password(password: &str, phc_salt_string: &SaltString) -> String {
         // resources used:
         // modern rust hashing guide: https://www.lpalmieri.com/posts/password-authentication-in-rust/
         // argon2 docs: https://docs.rs/argon2/latest/argon2/
@@ -68,15 +68,12 @@ impl Credentials {
         let password = password.as_bytes();
 
         // Argon2 with OWASP params
-        let hasher = Argon2::new(
-            Algorithm::Argon2id,
-            Version::V0x13,
-            Params::new(15000, 2, 1, None).expect("build Argon2 params"),
-        );
+        let params = Params::new(15000, 2, 1, None).expect("build Argon2 params");
+        let hasher = Argon2::new(Algorithm::Argon2id, Version::V0x13, params);
 
         // Hash password to PHC string ($argon2id$v=19$...)
         let password_hash = hasher
-            .hash_password(password, salt_string)
+            .hash_password(password, phc_salt_string)
             .expect("hash password");
         password_hash.to_string()
     }
