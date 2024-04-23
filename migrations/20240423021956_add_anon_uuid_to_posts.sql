9defaa835ab18f10bb662cf3336df47aeccf3d33
Kriemhild Gretchen <madoka.karamazov@gmail.com>
2024-04-22 19:56:20 -0700

    update old anon posts to have an anon_uuid

--- migrations/20240423021956_add_anon_uuid_to_posts.sql
+++ migrations/20240423021956_add_anon_uuid_to_posts.sql
@@ -2,6 +2,7 @@ BEGIN;
 ALTER TABLE posts ALTER status SET DEFAULT 'pending';
 UPDATE posts SET status = 'pending' WHERE status = 'new';
 ALTER TABLE posts ADD anon_uuid char(36);
+UPDATE posts SET anon_uuid = gen_random_uuid() WHERE user_id IS NULL;
 CREATE INDEX ON posts (anon_uuid);
 CREATE INDEX ON posts (user_id);
 COMMIT;
