ccc10724c192aa5639515748957d45e8b7d923c0
Kriemhild Gretchen <madoka.karamazov@gmail.com>
2024-05-04 14:48:11 -0700

    remove db dir
    it was only used for hosting sql dumps. instead, simply delete the
    sql dump immediately after it is transferred or loaded. it will be
    kept in the project root during its brief existence.

--- script/reload.sh
+++ script/reload.sh
@@ -4,8 +4,10 @@ if ! [ "$DEV" == 1 ]; then
     exit 1
 fi
 ssh $SSH_APP "cd schiz && script/snap.sh"
-scp $SSH_APP:schiz/db/schiz.sql.gz db
-gunzip --force db/schiz.sql.gz
+scp $SSH_APP:schiz/schiz.sql.gz .
+ssh $SSH_APP "rm schiz/schiz.sql.gz"
+gunzip --force schiz.sql.gz
 dropdb schiz
 createdb schiz -O schiz
-psql $DATABASE_URL < db/schiz.sql
+psql $DATABASE_URL < schiz.sql
+rm schiz.sql
--- script/snap.sh
+++ script/snap.sh
@@ -1,3 +1,3 @@
 set -x
-nice pg_dump $DATABASE_URL > db/schiz.sql
-nice gzip --force db/schiz.sql
+nice pg_dump $DATABASE_URL > schiz.sql
+nice gzip --force schiz.sql
