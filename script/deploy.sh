ecc6b65c91d191c62c3dc11ba9a90dd09b5c0965
Kriemhild Gretchen <madoka.karamazov@gmail.com>
2024-05-04 18:47:05 -0700

    only stop the app when running database migration
    no reason to stop it just for building, pulling from git, or upgrading
    rust.
    
    update the 502 message to explain this purpose.
    
    unrelated, but remove "--bins" from cargo test because we do not have
    any integration tests currently (or any tests at all, for that matter).

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,11 +3,11 @@ if ! [ "$DEV" == 1 ]; then
     git pull
     nice ../.cargo/bin/rustup upgrade
     nice ../.cargo/bin/cargo build --release
-    nice ../.cargo/bin/sqlx migrate run
 else
-    cargo test --bins
+    cargo test
     git push
-    ssh $SSH_SUDO "sudo systemctl stop schiz"
     ssh $SSH_APP "cd schiz && script/deploy.sh"
+    ssh $SSH_SUDO "sudo systemctl stop schiz"
+    ssh $SSH_APP "cd schiz && ../.cargo/bin/sqlx migrate run"
     ssh $SSH_SUDO "sudo systemctl start schiz"
 fi
