400f23dd8e32acd90b75fc78196c056c0609b1e8
Kriemhild Gretchen <madoka.karamazov@gmail.com>
2024-05-04 18:34:51 -0700

    add a maintenance page for deployments
    stop the service and restart it after deployment is complete.

--- script/deploy.sh
+++ script/deploy.sh
@@ -7,6 +7,7 @@ if ! [ "$DEV" == 1 ]; then
 else
     cargo test --bins
     git push
+    ssh $SSH_SUDO "sudo systemctl stop schiz"
     ssh $SSH_APP "cd schiz && script/deploy.sh"
-    ssh $SSH_SUDO "sudo systemctl restart schiz"
+    ssh $SSH_SUDO "sudo systemctl start schiz"
 fi
