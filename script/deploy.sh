48364577925cb33ec149099138ac1aa3d0a087e6
Kriemhild Gretchen <madoka.karamazov@gmail.com>
2024-05-19 01:37:50 -0700

    add ~/.cargo/bin to PATH in deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,13 +1,14 @@
 set -xe
 if ! [ "$DEV" == 1 ]; then
+    PATH="$HOME/.cargo/bin:$PATH"
     git pull
-    nice ../.cargo/bin/rustup upgrade
-    nice ../.cargo/bin/cargo build --release
+    nice rustup upgrade
+    nice cargo build --release
 else
     cargo test
     git push
     ssh $APP_SSH_ACCOUNT "cd schiz && script/deploy.sh"
     ssh $SUDO_SSH_ACCOUNT "sudo systemctl stop apabbs"
-    ssh $APP_SSH_ACCOUNT "cd schiz && ../.cargo/bin/sqlx migrate run"
+    ssh $APP_SSH_ACCOUNT "cd schiz && ~/.cargo/bin/sqlx migrate run"
     ssh $SUDO_SSH_ACCOUNT "sudo systemctl start apabbs"
 fi
