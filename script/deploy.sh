dcfdecb2bd6c0acc36f9f5f74ed8f08aa7898983
Kriemhild Gretchen <madoka.karamazov@gmail.com>
2024-05-18 20:34:20 -0700

    rename systemd service to apabbs

--- script/deploy.sh
+++ script/deploy.sh
@@ -7,7 +7,7 @@ else
     cargo test
     git push
     ssh $APP_SSH_ACCOUNT "cd schiz && script/deploy.sh"
-    ssh $SUDO_SSH_ACCOUNT "sudo systemctl stop schiz"
+    ssh $SUDO_SSH_ACCOUNT "sudo systemctl stop apabbs"
     ssh $APP_SSH_ACCOUNT "cd schiz && ../.cargo/bin/sqlx migrate run"
-    ssh $SUDO_SSH_ACCOUNT "sudo systemctl start schiz"
+    ssh $SUDO_SSH_ACCOUNT "sudo systemctl start apabbs"
 fi
