3413523981606b44fa248d6a1c8234398eb87fbe
Kriemhild Gretchen <madoka.karamazov@gmail.com>
2024-08-23 21:46:13 -0700

    bump version to 0.8.3

--- migrations/20240824043859_add_lazy_loading.sql
+++ migrations/20240824043859_add_lazy_loading.sql
@@ -1,1 +1,7 @@
-UPDATE posts SET body = REPLACE(body, 'title="YouTube video player"', 'loading="lazy" title="YouTube video player"') WHERE body LIKE '%title="YouTube video player"%';
+UPDATE posts SET body =
+    REPLACE(
+        body,
+        'title="YouTube video player"',
+        'loading="lazy" title="YouTube video player"'
+    )
+    WHERE body LIKE '%title="YouTube video player"%';
