93c524bb96eecf38d8c80b0010b8eedeb3a3cb3e
Kriemhild Gretchen <madoka.karamazov@gmail.com>
2024-08-29 21:53:40 -0700

    tweak crypto.rs 'use' statements and comments

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,20 +1,24 @@
+use argon2::{
+    password_hash::{rand_core::OsRng, PasswordHasher, SaltString},
+    Algorithm, Argon2, Params, Version,
+};
+
 // PHC salt string used in password hashing
-use argon2::password_hash::SaltString;
 
 pub fn generate_phc_salt_string() -> SaltString {
-    use argon2::password_hash::rand_core::OsRng;
     SaltString::generate(&mut OsRng)
 }
 
+// Convert Base64 salt string to PHC SaltString
+
 pub fn convert_b64_salt(b64_salt: &str) -> SaltString {
-    SaltString::from_b64(b64_salt).expect("convert B64 str to PHC SaltString")
+    SaltString::from_b64(b64_salt).expect("convert Base64 str to PHC SaltString")
 }
 
 pub fn hash_password(password: &str, phc_salt_string: &SaltString) -> String {
     // resources used:
     // modern rust hashing guide: https://www.lpalmieri.com/posts/password-authentication-in-rust/
     // argon2 docs: https://docs.rs/argon2/latest/argon2/
-    use argon2::{password_hash::PasswordHasher, Algorithm, Argon2, Params, Version};
     let password = password.as_bytes();
 
     // Argon2 with OWASP params
