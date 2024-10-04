2a438c23c036e26485703e00a0b6e84e76506826
Kriemhild Gretchen <madoka.karamazov@gmail.com>
2024-10-03 21:24:41 -0700

    cargo update
    use new croner library (replacement for cron in tokio-cron-scheduler 0.12.0)

--- src/jobs.rs
+++ src/jobs.rs
@@ -1,11 +1,18 @@
-// sec   min   hour   day of month   month   day of week   year
-// *     *     *      *              *       *             *
+// ┌──────────────── second (0 - 59)
+// │ ┌────────────── minute (0 - 59)
+// │ │ ┌──────────── hour (0 - 23)
+// │ │ │ ┌────────── day of month (1 - 31)
+// │ │ │ │ ┌──────── month (1 - 12, JAN-DEC)
+// │ │ │ │ │ ┌────── day of week (0 - 6, SUN-Mon)
+// │ │ │ │ │ │       (0 to 6 are Sunday to Saturday; 7 is Sunday, the same as 0)
+// │ │ │ │ │ │
+// * * * * * *
 
 use crate::{ban, init, BEGIN, COMMIT};
 use tokio_cron_scheduler::Job;
 
 pub fn scrub_ips() -> Job {
-    Job::new_async("0 0 11 * * * *", |_uuid, _l| {
+    Job::new_async("0 0 11 * * *", |_uuid, _l| {
         Box::pin(async move {
             let db = init::db().await;
             let mut tx = db.begin().await.expect(BEGIN);
