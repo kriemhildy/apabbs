5668e0f7ae23571a92a73756eb8c2063780f9d8f
Kriemhild Gretchen <madoka.karamazov@gmail.com>
2025-03-25 21:54:55 -0700

    use div for youtube logo
    this is to avoid having to use break tags in the youtube div, as break tags
    are important for analyzing direct user input of newlines.

--- migrations/20250326004724_add_body_intro.sql
+++ migrations/20250326004724_add_body_intro.sql
@@ -1,2 +1,12 @@
+BEGIN;
+
 ALTER TABLE posts ADD COLUMN intro_limit_opt int
-    CHECK (intro_limit_opt >= 300 AND intro_limit_opt <= 2000);
+    CHECK (intro_limit_opt >= 100 AND intro_limit_opt <= 2000);
+
+UPDATE posts SET body = regexp_replace(
+    body,
+    '<div class="youtube">(.*?) class="logo" alt></a><br>',
+    '<div class="youtube"><div class="logo">\1 alt></a></div>'
+);
+
+COMMIT;
