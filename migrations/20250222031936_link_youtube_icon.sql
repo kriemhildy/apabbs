88342dbe2ddf00be85a44fd1aa6f119e14c37f1d
Kriemhild Gretchen <madoka.karamazov@gmail.com>
2025-04-11 00:32:23 -0700

    commit hacked migration for link_youtube_icon
    the checksum has been updated in production.

--- migrations/20250222031936_link_youtube_icon.sql
+++ migrations/20250222031936_link_youtube_icon.sql
@@ -4,15 +4,15 @@ UPDATE posts SET body = regexp_replace(
     body,
     concat(
         '<img class="youtube" src="/youtube.svg" alt>',
-        '<a href="https://www.youtube.com/watch\?v=(\w+)" target="_blank">',
-        '<img src="([^"]+)" alt="YouTube (\w+)">',
+        '<a href="https://www.youtube.com/watch\?v=([\w\-]{11})" target="_blank">',
+        '<img src="([^"]+)" loading="lazy">',
         '</a>'
     ),
     concat(
         '<a class="youtube" href="https://www.youtube.com/watch?v=\1" ',
         'target="_blank"><img src="/youtube.svg" alt>',
         '</a>',
-        '<img src="\2" alt="YouTube \3">'
+        '<img src="\2" alt="YouTube \1">'
     ),
     'g'
 ) WHERE body LIKE '%<img class="youtube" src="/youtube.svg" alt>%';
