531bff8860cddaf70bd86f310dafc1d6cf67faf1
Kriemhild Gretchen <madoka.karamazov@gmail.com>
2025-04-12 19:46:11 -0700

    combine migrate into same line of deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -20,7 +20,6 @@ else
     git push
     ssh $SSH_APP_USER "cd $SSH_APP_PATH && script/deploy.sh"
     ssh $SSH_SUDO_USER "sudo systemctl stop $SSH_SERVICE"
-    ssh $SSH_APP_USER "cd $SSH_APP_PATH && ~/.cargo/bin/sqlx migrate run"
-    ssh $SSH_APP_USER "cd $SSH_APP_PATH && target/release/migrate"
+    ssh $SSH_APP_USER "cd $SSH_APP_PATH && ~/.cargo/bin/sqlx migrate run && target/release/migrate"
     ssh $SSH_SUDO_USER "sudo systemctl start $SSH_SERVICE"
 fi
