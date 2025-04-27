c62d47cf61fbb0ae7158820dbcc99806e675c077
Kriemhild Gretchen <madoka.karamazov@gmail.com>
2025-04-27 02:54:37 -0700

    differentiate scrollbar track colors
    this only really happens on chrome, and it ruins my "marginless" effect.
    so i need to style it to make clear the scrollbar is outside my page margins.

--- pub/script.js
+++ pub/script.js
@@ -7,7 +7,7 @@ function styleScrollbar() {
         const primaryLink = document.querySelector("link[rel=stylesheet]");
         const scrollbarLink = document.createElement("link");
         scrollbarLink.rel = "stylesheet";
-        scrollbarLink.href = "/scrollbar.css?5";
+        scrollbarLink.href = "/scrollbar.css?6";
         console.log("non-mac webkit scrollbar");
         primaryLink.after(scrollbarLink);
     }
