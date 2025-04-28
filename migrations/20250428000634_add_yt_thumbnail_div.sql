7cff4640ef04de183438f5441aadbe1ba21ce78e
Kriemhild Gretchen <madoka.karamazov@gmail.com>
2025-04-27 20:27:21 -0700

    add youtube-thumbnail div
    mainly so we can control the css better later to avoid over-applying rules and
    using unset.

---
+++ migrations/20250428000634_add_yt_thumbnail_div.sql
@@ -0,0 +1,23 @@
+UPDATE posts SET body = regexp_replace(
+    body,
+    concat(
+        '<div class="youtube">' || E'\n',
+        '    <div class="youtube-logo">' || E'\n',
+        '        (.*?)' || E'\n',
+        '    </div>' || E'\n',
+        '    (.*?)' || E'\n',
+        '</div>'
+    ),
+    concat(
+        '<div class="youtube">' || E'\n',
+        '     <div class="youtube-logo">' || E'\n',
+        '          \1' || E'\n',
+        '     </div>' || E'\n',
+        '     <div class="youtube-thumbnail">' || E'\n',
+        '          \2' || E'\n',
+        '     </div>' || E'\n',
+        '</div>'
+    ),
+    'g'
+)
+WHERE body LIKE '%<div class="youtube">%';
