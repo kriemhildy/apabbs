31fe3ca50fbc6e875db294cb40af9b243f90659e
Kriemhild Gretchen <madoka.karamazov@gmail.com>
2025-07-22 14:36:04 -0700

    satisfy cargo clippy, move should_send logic
    now that we have a separate function for post_message_json, the should_send
    function being all the way in the post.rs model no longer makes sense.

--- src/router/posts.rs
+++ src/router/posts.rs
@@ -335,12 +335,22 @@ pub async fn hide_post(
 /// Returns post message JSON
 pub fn post_message_json(state: &AppState, post: &Post, user: &User) -> Option<serde_json::Value> {
     // Determine if this post should be sent to the user
-    if !post.should_send(&user) {
-        return None;
+    let should_send = post.author(user)
+        || match user.account {
+            Some(ref account) => match account.role {
+                AccountRole::Admin => true,
+                AccountRole::Mod => {
+                    !matches!(post.status, PostStatus::Rejected | PostStatus::Reported)
+                }
+                _ => post.status == PostStatus::Approved,
+            },
+            None => post.status == PostStatus::Approved,
+        };
+    if !should_send {
+        return None; // Skip sending this post
     }
-
     // Render post HTML and send as JSON
-    let html = match render(&state, "post.jinja", minijinja::context!(post, user)) {
+    let html = match render(state, "post.jinja", minijinja::context!(post, user)) {
         Ok(html) => html,
         Err(e) => {
             tracing::error!("Failed to render post for websocket: {:?}", e);
@@ -357,18 +367,12 @@ pub fn account_message_json(
     user: &User,
 ) -> Option<serde_json::Value> {
     // Send JS for adding pending accounts, removing pending accounts, and updating account owner view
-    // This should not take that long to implement, really.
-    if user.account.is_none() {
-        return None; // No account, no updates
-    }
-
-    let user_account = user.account.as_ref().unwrap();
-
+    let user_account = user.account.as_ref()?;
     if msg_account.id == user_account.id {
         Some(serde_json::json!({"username": msg_account.username}))
     } else if user_account.role == AccountRole::Admin {
         let html = match render(
-            &state,
+            state,
             "pending_account.jinja",
             minijinja::context!(account => msg_account, user),
         ) {
