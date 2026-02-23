213ba076fb9f562987a93af54bada753128d2e65
Kriemhild Gretchen <madoka.karamazov@gmail.com>
2026-02-22 20:53:07 -0800

    240 default fix

--- src/lib.rs
+++ src/lib.rs
@@ -31,7 +31,7 @@ pub fn per_page() -> usize {
         Ok(per_page) => per_page
             .parse()
             .expect("Parse PER_PAGE environment variable as integer"),
-        Err(_) => 420,
+        Err(_) => 240,
     }
 }
 
